93a6afb25ea4d4316b79f279b7983d5f1696a6fb
GGPC <GGPC@RAVEN>
2026-04-13 15:43:27 +1200

    Add project files.

---
+++ F1 Car Garage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace F1_Car_Garage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
